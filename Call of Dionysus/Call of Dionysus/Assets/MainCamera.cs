1a1abb00221a23a0897f3ea91ab537d85719f503
samanthemum <sambhallam@gmail.com>
2022-03-08 12:24:24 -0600

    Added more sanity effects
    Added sanity camera effects

--- Call of Dionysus/Call of Dionysus/Assets/MainCamera.cs
+++ Call of Dionysus/Call of Dionysus/Assets/MainCamera.cs
@@ -9,10 +9,18 @@ public class MainCamera : MonoBehaviour
     private Rigidbody2D heroRB;
     private HeroScript heroScript;
     private float speedFactor = .5f;
+    private float defaultFOV = 5;
+    private Camera camera;
+    private AudioListener audioListener;
+    private float rateOfZoom = 5;
+    private float rateOfMirror = 5;
+    private float rateOfAudio = 10;
     void Start()
     {
         heroRB = hero.GetComponent<Rigidbody2D>();
         heroScript = hero.GetComponent<HeroScript>();
+        camera = this.GetComponent<Camera>();
+        audioListener = this.GetComponent<AudioListener>();
     }
 
     // Update is called once per frame
@@ -27,6 +35,66 @@ public class MainCamera : MonoBehaviour
         } else if(heroScript.getSanity() <= 80)
         {
             GetComponent<RectTransform>().position = Vector3.Lerp(GetComponent<RectTransform>().position, new Vector3(heroRB.transform.position.x, heroRB.transform.position.y, -2.5f), Time.deltaTime*speedFactor);
+        }
+
+        // do camera zoom stuff
+        if(heroScript.getSanity() <= 60)
+        {
+            rateOfZoom -= Time.deltaTime;
+            if(rateOfZoom <= 0)
+            {
+                rateOfZoom = 5;
+                float doZoom = Random.Range(1, 10);
+
+                if(doZoom <= 5)
+                {
+                    camera.orthographicSize = 2.5f;
+                } else
+                {
+                    camera.orthographicSize = defaultFOV;
+                }
+            }
+        }
+
+        // do camera flip stuff
+        if (heroScript.getSanity() <= 40)
+        {
+            rateOfMirror -= Time.deltaTime;
+            if (rateOfMirror <= 0)
+            {
+                rateOfMirror = 5;
+                float doMirror = Random.Range(1, 10);
+
+                if (doMirror <= 5)
+                {
+                    camera.projectionMatrix = camera.projectionMatrix * Matrix4x4.Scale(new Vector3(-1, 1, 1));
+                }
+                else
+                {
+                    camera.ResetProjectionMatrix();
+                }
+            }
+        }
+
+        // do audio stuff
+        if (heroScript.getSanity() <= 20)
+        {
+            rateOfAudio -= Time.deltaTime;
+            if (rateOfAudio <= 0)
+            {
+                rateOfAudio = 10;
+                float doAudioCut = Random.Range(1, 10);
+
+                if (doAudioCut <= 5)
+                {
+                    audioListener.enabled = false;
+                }
+                else
+                {
+                    camera.ResetProjectionMatrix();
+                    audioListener.enabled = true;
+                }
+            }
         }
     }
 }
